3a4087afbac3d79964aed4da8d00f6e219294ce0
xiaofuyesnew <xiaofuyesnew@qq.com>
2016-07-15 00:15:46 +0800

    nature

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,13 @@ function bmi(weight,height) {
     var bmi=weight/(height*height);
 
 }
+
+/**
+ * 选择模块，用于选择参数状态
+ */
+function choice(){
+    
+    var weightUnit=document.getElementById("weight_unit");
+    var heightUnit=document.getElementById("height_unit");
+    
+}
