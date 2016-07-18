bec197d682a41faab457673e8f976572a73a45a1
xiaofuyesnew <xiaofuyesnew@qq.com>
2016-07-18 22:51:18 +0800

    choice fuction

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,39 @@
 'use strict';
 /**
+ * 全局变量
+ */
+var weightStatGJ=true;
+var heightStatM=true;
+
+/**
+ * 选择模块，用于选择参数状态
+ */
+function choice(){
+    
+    var weightUnit=document.getElementById('weight_unit');
+    var jin=document.getElementById('jin');
+    var gongjin=document.getElementById('gongjin');
+    jin.onclick=function(){
+        weightUnit.innerHTML='斤';
+        weightStatGJ=false;
+    }
+    gongjin.onclick=function(){
+        weightUnit.innerHTML='公斤';
+        weightStatGJ=true;
+    }
+
+
+
+
+
+
+    var heightUnit=document.getElementById("height_unit");
+
+}
+
+choice();
+
+/**
  * 核心算法，bmi的计算函数
  * 
  */
@@ -7,14 +41,4 @@ function bmi(weight,height) {
     
     var bmi=weight/(height*height);
 
-}
-
-/**
- * 选择模块，用于选择参数状态
- */
-function choice(){
-    
-    var weightUnit=document.getElementById("weight_unit");
-    var heightUnit=document.getElementById("height_unit");
-    
 }
